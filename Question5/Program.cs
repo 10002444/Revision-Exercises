dcbe49fc8988e3379e2593b68ce571d5b5a054b3
Marisa Wadsworth <marisawadsworth@Marisas-MacBook-Air.local>
2019-03-01 14:28:01 +1300

    Question 5 done

---
+++ Question5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Question5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x,j;
+
+            Console.WriteLine("Enter a number to get a multiplication table up to 10. \n");
+
+            Console.Write("Input the number (Table to be calculated) : ");
+            x = Convert.ToInt32(Console.ReadLine());
+            for (j = 1; j <= 10; j++)
+            {
+                Console.Write("{0} + {1} = {2} \n",x,j,x*j);
+            }
+        }
+    }
+}
