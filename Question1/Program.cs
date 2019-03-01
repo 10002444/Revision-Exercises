05cfc71f15823c4f744f4762e49f0787e591d57f
Marisa Wadsworth <marisawadsworth@Marisas-MacBook-Air.local>
2019-03-01 13:15:14 +1300

    Question 1 and 2 finish

---
+++ Question1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Hamiora Taurima");
+            Console.ReadLine();
+        }
+    }
+}
