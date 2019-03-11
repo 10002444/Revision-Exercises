9e287bc75ad684ff41e5cadc5e9a9f0072451ddb
Marisa Wadsworth <marisawadsworth@Marisas-MacBook-Air.local>
2019-03-11 15:17:04 +1300

    Question 6 Start

---
+++ Question6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Question6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x; 
+
+            Console.Write("Input a number of the radius to find the area and perimeter of a circle (Number of Radius) : ");
+
+
+        }
+    }
+}
