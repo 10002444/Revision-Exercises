bee451e9438b1574227812e9b7fa9515cc28a466
Marisa Wadsworth <marisawadsworth@Marisas-MacBook-Air.local>
2019-03-11 15:33:46 +1300

    Question 6 Done

--- Question6/Program.cs
+++ Question6/Program.cs
@@ -6,11 +6,14 @@ namespace Question6
     {
         static void Main(string[] args)
         {
-            int x; 
+            //int x; 
 
-            Console.Write("Input a number of the radius to find the area and perimeter of a circle (Number of Radius) : ");
-
-
+            Console.WriteLine("Input a number of the radius to find the area and perimeter of a circle.");
+            double radius = double.Parse(Console.ReadLine());
+            double pi = Math.PI;
+            double area = 2 * pi * radius;
+            Console.WriteLine("The Perimeter or Circumference (C=2πr) of your circle is: {0:F2}", area);
+            
         }
     }
 }
